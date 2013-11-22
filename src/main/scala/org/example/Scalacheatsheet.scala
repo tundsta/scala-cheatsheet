604a163d07b5c61b96f5fb27e99ba2021011b6aa
tundi <tundi@bulbview.com>
2013-11-22 20:00:07 +0000

    add element to immutable map

--- src/main/scala/org/example/Scalacheatsheet.scala
+++ src/main/scala/org/example/Scalacheatsheet.scala
@@ -2,4 +2,11 @@ package org.example
 
 object Scalacheatsheet extends App {
   println("Hello, scala-cheatsheet")
+
+  println("add element to immutable Map & k/v replacement")
+  val m1 = Map(1 -> 2, 3 -> 4, 5 -> 6)
+  println(m1)
+  //res0: scala.collection.immutable.Map[Int,Int] = Map(1 -> 2, 3 -> 4, 5 -> 6)
+  println(m1 + (1 -> 3))
+
 }
