6bda0a44d8936639fad14ee849706dc40a3b86f1
tundi <tundi@bulbview.com>
2013-11-23 15:24:35 +0000

    example of collect and partial function

--- src/main/scala/org/example/Scalacheatsheet.scala
+++ src/main/scala/org/example/Scalacheatsheet.scala
@@ -1,12 +1,17 @@
 package org.example
 
-object Scalacheatsheet extends App {
-  println("Hello, scala-cheatsheet")
-
-  println("add element to immutable Map & k/v replacement")
+object Collections extends App {
+  println("===========================\nadd element to immutable Map & k/v replacement")
   val m1 = Map(1 -> 2, 3 -> 4, 5 -> 6)
   println(m1)
   //res0: scala.collection.immutable.Map[Int,Int] = Map(1 -> 2, 3 -> 4, 5 -> 6)
   println(m1 + (1 -> 3))
 
+  println("============================\nusing collect to filter a collection")
+  val elements = Seq("a", "bb", "cc", "ddd")
+  println(elements)
+  def accept = (s: String) => s.length > 1
+  def transform = (s: String) => s.toUpperCase
+  val collectResult = elements collect { case se: String if accept(se) => transform(se) }
+  println(collectResult)
 }
