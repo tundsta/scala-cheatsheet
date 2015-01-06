17f27baeeb419bb83e54abcef7865fbfd2656248
tundi <tundi@bulbview.com>
2015-01-06 13:25:23 +0000

    more function syntactic sugar examples

--- src/main/scala/functionLiteralDeclaration.scala
+++ src/main/scala/functionLiteralDeclaration.scala
@@ -1,6 +1,8 @@
 val isX = (_:String) == "x"
 val f = (_: Int) + (_: Int)
 val f = (i: Int) => { i % 2 == 0 }
+val asInt = (_:String).toInt
+() => { System.getProperty("user.dir") }
 val f: (Int) => Boolean = i => { i % 2 == 0 } 
 val f: Int => Boolean = i => { i % 2 == 0 } 
 val f: Int => Boolean = i => i % 2 == 0
