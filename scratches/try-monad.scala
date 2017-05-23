38d121acc856a4f280ae26285ab388b2ad756bc6
tundi <tundi@bulbview.com>
2017-05-23 07:51:26 +0100

    update

--- scratches/try-monad.scala
+++ scratches/try-monad.scala
@@ -2,18 +2,18 @@ import scala.util.{Failure, Success, Try}
 
 //Try/ Monad
 //Try will let you recover from exceptions at any point in the chain, so you can defer recovery to the end
- 
+
 val sum = (for {
   int1 <- Try(Integer.parseInt("one"))
   int2 <- Try(Integer.parseInt("two"))
 } yield {
-    int1 + int2
-  }) recover {
-    case e => 0
-  }
+  int1 + int2
+}) recover {
+  case e => 0
+}
 //sum: scala.util.Try[Int] = Success(0)
- 
- 
+
+
 //Processing a seq of Trys
 val maybeInts:Seq[scala.util.Try[Int]] = List(
   Success(1),
@@ -23,3 +23,4 @@ val maybeInts:Seq[scala.util.Try[Int]] = List(
   Success(4))
 maybeInts map { case Success(s) => s"int found: $s"; case Failure(f) => s"int error: $f" }
 // returns List(int found: 1, int found: 2, int error: java.lang.NumberFormatException: For input string: "a", int found: 3, int found: 4)
+maybeInts.d
