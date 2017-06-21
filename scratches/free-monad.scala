09bc46eec3f2746c142d861226109e6ee2e95a64
tundi <tundi@bulbview.com>
2017-06-21 08:13:34 +0100

    update

--- scratches/free-monad.scala
+++ scratches/free-monad.scala
@@ -5,3 +5,11 @@
 sealed trait Free[F[_], A]
 final case class Point[F[_], A](a: A) extends Free[F, A]
 final case class Join[F[_], A](s: F[Free[F, A]]) extends Free[F, A]
+
+
+//Now recall that a monad is defined by two operations,
+//Point is not very interesting  it just wraps a monad around a value.
+def point[M[_], A](a: A): M[A]
+//FlatMap is, however, the distinguishing feature of a monad and it tells us something very important:
+// monads are fundamentally about control flow."
+def flatMap[M[_], A, B](fa: M[A])(f: A => M[B]): M[B]
