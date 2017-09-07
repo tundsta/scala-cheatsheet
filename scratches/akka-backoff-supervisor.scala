3672e6fe2114b2382522fd1710cbdac060fae294
tundi <tundi@bulbview.com>
2017-09-07 19:33:15 +0100

    akka backoff sup.

---
+++ scratches/akka-backoff-supervisor.scala
@@ -0,0 +1,21 @@
+// an exponential-backoff-and-retry supervisor-actor can be used to
+// supervise the simulated wind-turbine actor.
+// A backoff-and-retry supervision-strategy is not a pattern offered by
+// the Akka Streams API, but it is a pattern offered by Akka Actors.
+
+import akka.actor.ActorSystem
+import akka.pattern.BackoffSupervisor
+
+implicit val system = ActorSystem()
+val id = 123
+
+val supervisor = BackoffSupervisor.props(
+  Backoff.onFailure(
+    someactor.props(id, Config.endpoint),
+    childName = id,
+    minBackoff = 1 second,
+    maxBackoff = 30 seconds,
+    randomFactor = 0.2
+  ))
+
+system.actorOf(supervisor, name = s"$id-backoff-supervisor")
