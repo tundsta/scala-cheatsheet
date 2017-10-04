905ec908952ad4adac31e4d8af4e38dfdca023a7
tundi <tundi@bulbview.com>
2017-10-04 13:01:45 +0100

    updated

--- scratches/try-monad.scala
+++ scratches/try-monad.scala
@@ -21,9 +21,37 @@ val maybeInts: Seq[scala.util.Try[Int]] = List(
   Failure(new java.lang.NumberFormatException("""For input string: "a""")),
   Success(3),
   Success(4))
+
 maybeInts map {
   case Success(s) => s"int found: $s";
   case Failure(f) => s"int error: $f"
 }
+
+
 // returns List(int found: 1, int found: 2, int error: java.lang.NumberFormatException: For input string: "a", int found: 3, int found: 4)
-maybeInts
+val fold = maybeInts.map{ x => x.fold(e => s"int error: $e", i => s"int found: $i") }
+
+maybeInts.flatMap(_.recover())
+
+//try in the middle
+
+//Or recover in the middle:
+
+val sum = for {
+  int1 <- Try(Integer.parseInt("one")).recover { case _ => 0 }
+  int2 <- Try(Integer.parseInt("2"))
+} yield {
+  int1 + int2
+}
+
+val sum2 = for {
+  int1 <- Try(Integer.parseInt("one")).recoverWith { case _ => Success(0) }
+  int2 <- Try(Integer.parseInt("2"))
+} yield {
+  int1 + int2
+}
+
+
+Try(Integer.parseInt("one")).fold(e => s"error: $e" , s => s"done $s")
+
+
